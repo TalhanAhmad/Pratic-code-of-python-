d9225eaff34495b55a07ba8b1774921a84ac8856
Talha ahmad awan <zablerkhanahmad@gmail.com>
2026-09-09 22:24:06 +0500

    Update 9th.py

--- 9th.py
+++ 9th.py
@@ -2,6 +2,6 @@ a = int(input("The first number is 1:",))
 # b = int(input("The second number is 2:",))
 
 print("square of the number is ", a**2)
-
+print
 
 # finding square a^2 and a*2 is not valid
